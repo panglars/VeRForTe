1b7fe2b14e8a4b9578887f8e0ce6549077792fe0
Kevin.MX <zhengjingkun@iscas.ac.cn>
2025-04-10 14:08:20 +0800

    chore: fix typo

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "RuyiSDK",
   url: "https://matrix.ruyisdk.org",
   title: "RISC-V Board and OS Support Matrix",
-  title_zh: "RISC-V 开发版和操作系统支持矩阵",
+  title_zh: "RISC-V 开发板和操作系统支持矩阵",
   links: {
     github: "https://github.com/ruyisdk/support-matrix",
     ruyisdk: "https://ruyisdk.org/",
